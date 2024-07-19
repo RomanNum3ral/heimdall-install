9779258a8f6e2bcc5e230c826be79c9a61e5f66a
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-07-19 12:41:59 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Install Heimdall
+
+mkdir /home/kodestar/docker
+sudo docker run --name=heimdall --restart always -d -v /home/kodestar/docker/heimdall:/config -e PGID=1000 -e PUID=1000 -p 8080:80 -p 8443:443 linuxserver/heimdall
